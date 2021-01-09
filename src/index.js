e83fed82a3beb07dc93b329c3442f3e0d0b10644
afeiship <1290657123@qq.com>
2021-01-09 15:40:51 +0800

    fix: global ctx error with typeof

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 (function () {
-  var global = global || this || window || Function('return this')();
+  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
 
   var nxPathParse = require('@jswork/next-path-parse');
